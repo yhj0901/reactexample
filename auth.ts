8206e5f68c56a6d1292982f2f901004e77520966
양희준 <yangheejune0901@gmail.com>
2024-06-11 12:26:56 +0900

    [Improvement] next-auth 설정

--- auth.ts
+++ auth.ts
@@ -12,7 +12,7 @@ export const config = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
     }),
   ],
-  secret: process.env.NEXT_PUBLIC_AUTH_SECRET,
+  secret: process.env.AUTH_SECRET,
   callbacks: {
     authorized({ request, auth }) {
       const { pathname } = request.nextUrl;
